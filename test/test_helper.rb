fa545ea6eeda351d54751cbc767b76c43a22d4f7
John Weir <john@smokinggun.com>
2015-02-04 11:48:32 -0500

    auto start Redis when running the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,9 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 require './lib/goalkeeper'
+
+require './test/support/redis_instance'
+
+# Start up Redis on a random port
+Goalkeeper.redis = RedisInstance.run!
+
